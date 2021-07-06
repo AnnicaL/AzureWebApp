276e1a41a2b4ed367df3a025c06d2d2fdb26bca6
Annica Landbü <annica.landbu@hm.com>
2021-07-06 11:10:24 +0200

    xxx

--- src/main/java/Group3/AzureWebApp/AzureWebAppApplication.java
+++ src/main/java/Group3/AzureWebApp/AzureWebAppApplication.java
@@ -3,13 +3,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Value;
 
+
+
 @SpringBootApplication
 public class AzureWebAppApplication {
 
-	@Value("${connectionString2}")
-	private String connectionString;
+	/*@Value("${connectionString2}")
+	private String connectionString;*/
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(AzureWebAppApplication.class, args);
 	}
+
 
 }
