482c7957bcd54ffc4bb5189bbaad625527fddda0
Hakan Danielsson <hakan.danielsson@hm.com>
2021-07-06 15:12:29 +0200

    Something

--- src/main/java/Group3/AzureWebApp/AzureWebAppApplication.java
+++ src/main/java/Group3/AzureWebApp/AzureWebAppApplication.java
@@ -1,10 +1,13 @@
 package Group3.AzureWebApp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AzureWebAppApplication {
+	@Value("${connectionString2}")
+    private String connectionString;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureWebAppApplication.class, args);
